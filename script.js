e367f6d656d1fcb4b3c13ccbd02ec8f2442e3a38
Wanna Maise <a82952@ualg.pt>
2024-04-21 18:11:01 +0100

    test

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $('.sub-menu ul').hide();
 $(".sub-menu a").click(function () {
   $(this).parent(".sub-menu").children("ul").slideToggle("100");
-  $(this).parent(".sub-sub-menu").children("ul").slideToggle("100");
   $(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
 });

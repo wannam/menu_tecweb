1d97d612fbfa0f6614481ff8c9a4f3c23a6404a1
Wanna Maise <a82952@ualg.pt>
2024-04-21 18:03:03 +0100

    tets menu

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $('.sub-menu ul').hide();
 $(".sub-menu a").click(function () {
   $(this).parent(".sub-menu").children("ul").slideToggle("100");
+  $(this).parent(".sub-sub-menu").children("ul").slideToggle("100");
   $(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
 });

0aae3a4d0f7db97a8bcabf7f51d9fcddb887a3ec
Wanna Maise <a82952@ualg.pt>
2024-04-21 18:35:42 +0100

    deleted

--- script.js
+++
@@ -1,5 +0,0 @@
-$('.sub-menu ul').hide();
-$(".sub-menu a").click(function () {
-  $(this).parent(".sub-menu").children("ul").slideToggle("100");
-  $(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
-});

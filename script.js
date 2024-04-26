47e5f2d68fb304f10a9fe630ac61c836a0f3487b
Wanna Maise <a82952@ualg.pt>
2024-04-26 21:20:49 +0100

    cokiee script

--- script.js
+++ script.js
@@ -4,42 +4,4 @@ $(".sub-menu a").click(function () {
   $(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
 });
 
-/*cookie*/
-
-window.onload = function () {
-  if (!getCookie('cookieConsent')) {
-      document.getElementById('cookiePopup').style.display = 'block';
-  }
-}
-
-function acceptCookies() {
-  setCookie('cookieConsent', 'true', 30);
-  document.getElementById('cookiePopup').style.display = 'none';
-}
-
-function closePopup() {
-  document.getElementById('cookiePopup').style.display = 'none';
-}
-
-function setCookie(name, value, days) {
-  var expires = "";
-  if (days) {
-      var date = new Date();
-      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-      expires = "; expires=" + date.toUTCString();
-  }
-  document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
-
-function getCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-  }
-  return null;
-}
-
 
